23f0e7e823a4cfeba9a70e9fa5fdb0dc4a0e5180
Hitha <hithabadikillaya@gmail.com>
2026-01-27 11:49:22 +0530

    chore: setup expo and integrate firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: Constants.manifest.extra.FIREBASE_API_KEY,
+    authDomain: Constants.manifest.extra.FIREBASE_AUTH_DOMAIN,
+    projectId: Constants.manifest.extra.FIREBASE_PROJECT_ID,
+    storageBucket: Constants.manifest.extra.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: Constants.manifest.extra.FIREBASE_MESSAGING_SENDER_ID,
+    appId: Constants.manifest.extra.FIREBASE_APP_ID,
+    measurementId: Constants.manifest.extra.FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
