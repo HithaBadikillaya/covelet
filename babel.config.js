8fa9ca3e3a194f100d2a9b32ef55a6499f647d63
Hitha <hithabadikillaya@gmail.com>
2026-03-20 20:26:05 +0530

    chore: the last resort

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,22 @@
 module.exports = function (api) {
   api.cache(true);
+
+  // Load and modify the nativewind preset function
+  const nativewindPreset = require("nativewind/babel");
+  const nativewindConfig = nativewindPreset(api);
+
+  // Filter out the missing worklets plugin from the results
+  if (nativewindConfig.plugins) {
+    nativewindConfig.plugins = nativewindConfig.plugins.filter(
+      (plugin) => plugin !== "react-native-worklets/plugin"
+    );
+  }
+
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+      nativewindConfig,
     ],
-    plugins: [
-      "react-native-reanimated/plugin",
-      [
-        "module-resolver",
-        {
-          alias: {
-            "react-native-worklets": "react-native-worklets-core",
-          },
-        },
-      ],
-    ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
